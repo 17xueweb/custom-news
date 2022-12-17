f19cc584642fd9fcb4aff724a9425343b5791916
lcruan <1416802331@qq.com>
2022-12-17 16:50:01 +0800

    切换选项卡数据懒加载

--- components/list/list.vue
+++ components/list/list.vue
@@ -1,7 +1,7 @@
 <template>
   <swiper class="home-swiper" :current="activeIndex" @change="change">
     <swiper-item v-for="(item, index) in tab" :key="index" class="swiper-item">
-      <list-item :list="list"></list-item>
+      <list-item :list="listCatchData[index]"></list-item>
     </swiper-item>
   </swiper>
 </template>
@@ -27,22 +27,40 @@
     name: "list",
     data() {
       return {
-        list: []
+        list: [],
+        // 对数据进行缓存
+        listCatchData: {}
       };
+    },
+    watch: {
+      tab(newVal) {
+        if(newVal.length === 0) return
+        this.getList(this.activeIndex)
+      }
     },
     // onLoad 在页面， created 在组件中
     created() {
-      this.getList()
+      // TODO: tab还没有赋值，所以传0 报错， 监听tab变化
+      // this.getList(0)
     },
     methods: {
       change(e) {
         const { current } = e.detail
+        this.getList(current)
         this.$emit('change', current)
+        
       },
-      getList() {
-        this.$api.get_list().then(res => {
+      getList(current) {
+        this.$api.get_list({
+          name: this.tab[current].name
+          }).then(res => {
           const { data } = res
-          this.list = data
+          console.log('请求数据：', data);
+          // this.list = data
+          // this.listCatchData[current] = data
+          // $set 帮我们通知页面 我们数组或者对象发生了变化 去刷新下
+          // 数据懒加载
+          this.$set(this.listCatchData, current, data)
         })
       }
     }
--- uniCloud-aliyun/cloudfunctions/get_list/index.js
+++ uniCloud-aliyun/cloudfunctions/get_list/index.js
@@ -2,12 +2,27 @@
 // 1. 获取数据库的引用
 const db = uniCloud.database()
 exports.main = async (event, context) => {
+  // 接收分类,通过分类去筛选数据 event可以接收参数
+  const { name } = event
+  
+  // 聚合：更精细化去处理数据 求和、分组、指定返回哪些字段
+  // aggregate 获取数据库集合的聚合操作实例
+  // match 根据条件过滤文档，并且把符合条件的文档传递给下一个流水线阶段,筛选classify 并且把name值传进来
+  // project 指定某些字段返回到数据中或者某些字段不去返回到数据中 跟下面的 field 使用方法很接近，不要content内容
+  // end 标志聚合操作定义完成，发起实际聚合操作
+  const list = await db.collection('article').aggregate().match({
+    classify: name
+  }).project({
+    content: false
+  }).end()
+  
+  
 
  // 2. 获取数据表 field过滤掉content字段
-  const list = await db.collection('article').field({
-    // true 表示只返回这个字段， false 表示不返回这个字段
-    content: false
-  }).get()
+  // const list = await db.collection('article').field({
+  //   // true 表示只返回这个字段， false 表示不返回这个字段
+  //   content: false
+  // }).get()
 	
 	//返回数据给客户端
 	return {
